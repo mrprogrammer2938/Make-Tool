c93475115fe89ac7a54d16f8e07d7448917d9458
Mr.nope <78996423+mrprogrammer2938@users.noreply.github.com>
2021-07-17 11:47:08 +0430

    Make-File
    make-file

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This Code write by Mr.nope
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "Please, Run This Programm as Root!"
+  echo ""
+  exit 1
+fi
+start() {
+    printf '\033]2;Make-File-Installing\a'
+    clear
+    echo "Installing..."
+    sleep 2
+    apt install python3
+    apt install python
+    apt install python3-pip
+    pip3 install --upgrade pip
+}
+chmod +x tool.py
+start
